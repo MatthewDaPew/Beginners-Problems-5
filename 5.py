3babf99ddfdbf941dcccd5b4d8b7603473695ba1
MatthewDaPew <162397704+MatthewDaPew@users.noreply.github.com>
2024-09-11 15:33:25 +1200

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,14 @@
+count = 0
+suslist = []
+
+for num in range (0,8):
+    baka = input("what do you say after sussy?"
+    ).lower()
+    if baka == "baka":
+        count += 1
+    else:
+        suslist.append(baka)
+        
+print(count, "people are sussy bakas")
+for num in range (0, count):
+    print("you sussy baka")
